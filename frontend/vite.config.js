35a21748e048ee9476ecedb7010658b692116ee5
EthanLy1 <ethanly.dev@gmail.com>
2026-05-20 13:56:41 +1000

    Updated syntax for Vite 8
    -fixed error on deployment
    -"Invalid type: Expected Function but received Object. "

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          leaflet: ['leaflet'],
-          recharts: ['recharts'],
-          vendor: ['react', 'react-dom'],
+        manualChunks(id) {
+          if (id.includes('leaflet')) return 'leaflet'
+          if (id.includes('recharts')) return 'recharts'
+          if (id.includes('react-dom') || id.includes('react/')) return 'vendor'
         }
       }
     }
